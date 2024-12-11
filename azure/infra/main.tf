27df40f4c91780ec75bbbde796f8e1c660457ef8
unknown <shariffarmaan65@gmail.com>
2024-12-11 17:57:24 +0530

    resource provider

--- azure/infra/main.tf
+++ azure/infra/main.tf
@@ -1,3 +1,13 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.44.1"
+    }
+  }
+}
+
 provider "azurerm" {
   features {
     resource_group {
